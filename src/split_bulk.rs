1c59083d8cce934b136e220ddc7a660faefc3bdd
Sigurd Sætherø Spangelo <sigurd dot spangelo at gmail dot com>
2025-11-05 04:29:23 +0100

    Fix all doctests

--- src/split_bulk.rs
+++ src/split_bulk.rs
@@ -7,6 +7,26 @@ where
     type Left: Bulk<Item = Self::Item>;
     type Right: Bulk<Item = Self::Item>;
 
+    /// Splits a bulk in two at a specified index.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// # #![feature(generic_const_exprs)]
+    /// use bulks::*;
+    /// 
+    /// let a = b"leftright";
+    /// 
+    /// let (a1, a2) = a.bulk()
+    ///     .copied()
+    ///     .split_at([(); 4]);
+    /// 
+    /// let left = a1.collect::<[_; _]>();
+    /// let right = a2.collect::<[_; _]>();
+    /// 
+    /// assert_eq!(&left, b"left");
+    /// assert_eq!(&right, b"right");
+    /// ```
     #[track_caller]
     fn split_at(self, n: L) -> (Self::Left, Self::Right)
     where
