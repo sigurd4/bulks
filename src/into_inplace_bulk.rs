5cc77343e52781a64b7d0f8979d87deed1ea7632
sigurd4 <sigurd-s@online.no>
2026-06-07 21:54:48 +0200

    Work in progress

--- src/into_inplace_bulk.rs
+++ src/into_inplace_bulk.rs
@@ -1,21 +1,34 @@
 use crate::{InplaceBulk, IntoBulk};
 
-pub const trait IntoInplaceBulk
+pub trait IntoInplaceBulk: IntoBulk
 {
     type IntoInplaceBulk: InplaceBulk;
 
     fn into_inplace_bulk(self) -> Self::IntoInplaceBulk;
 }
-impl<I> const IntoInplaceBulk for I
+#[cfg(feature = "alloc")]
+impl<I> IntoInplaceBulk for I
 where
-    I: ~const private::_IntoInplace<_IntoInplace: ~const IntoBulk<IntoBulk: InplaceBulk>>
+    I: IntoBulk
 {
-    type IntoInplaceBulk = <<I as private::_IntoInplace>::_IntoInplace as IntoBulk>::IntoBulk;
+    type IntoInplaceBulk = <I as private::_IntoInplace>::_IntoInplace;
 
     fn into_inplace_bulk(self) -> Self::IntoInplaceBulk
     {
         self._into_inplace()
-            .into_bulk()
+    }
+}
+
+#[cfg(not(feature = "alloc"))]
+impl<I> IntoInplaceBulk for I
+where
+    I: IntoBulk<IntoBulk: InplaceBulk>
+{
+    type IntoInplaceBulk = <I as private::_IntoInplace>::_IntoInplace;
+
+    fn into_inplace_bulk(self) -> Self::IntoInplaceBulk
+    {
+        self._into_inplace()
     }
 }
 
@@ -25,67 +38,35 @@ mod private
 
     use crate::{CollectNearest, InplaceBulk, IntoBulk};
 
-    pub const trait _IntoInplace
+    pub trait _IntoInplace
     {
-        type _IntoInplace;
+        type _IntoInplace: InplaceBulk;
 
         fn _into_inplace(self) -> Self::_IntoInplace;
     }
-    impl<I> const _IntoInplace for I
+    #[cfg(feature = "alloc")]
+    impl<I> _IntoInplace for I
+    where
+        I: IntoBulk
     {
-        default type _IntoInplace = I;
+        default type _IntoInplace = <I::IntoBulk as CollectNearest>::Nearest;
 
         default fn _into_inplace(self) -> Self::_IntoInplace
         {
-            unsafe {
-                self.same()
-                    .unwrap_unchecked()
-            }
+            self.into_bulk()
+                .collect_nearest()
+                .into_bulk()
         }
     }
-    impl<I, B, N> const _IntoInplace for I
+    impl<I> _IntoInplace for I
     where
-        I: ~const IntoBulk<IntoBulk = B>,
-        B: ~const CollectNearest<Nearest = N>
+        I: IntoBulk<IntoBulk: InplaceBulk>
     {
-        type _IntoInplace = N;
+        type _IntoInplace = I::IntoBulk;
 
         fn _into_inplace(self) -> Self::_IntoInplace
         {
             self.into_bulk()
-                .collect_nearest()
-        }
-    }
-
-    pub const trait __IntoInplace
-    {
-        type __IntoInplace;
-
-        fn __into_inplace(self) -> Self::__IntoInplace;
-    }
-    impl<I> const __IntoInplace for I
-    {
-        default type __IntoInplace = <I as _IntoInplace>::_IntoInplace;
-
-        default fn __into_inplace(self) -> Self::__IntoInplace
-        {
-            unsafe {
-                self._into_inplace()
-                    .same()
-                    .unwrap_unchecked()
-            }
-        }
-    }
-    impl<I, B> const __IntoInplace for I
-    where
-        I: IntoBulk<IntoBulk = B>,
-        B: InplaceBulk
-    {
-        type __IntoInplace = I;
-
-        fn __into_inplace(self) -> Self::__IntoInplace
-        {
-            self
         }
     }
 }
