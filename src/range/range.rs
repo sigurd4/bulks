8186e6dd8df90b8056b0ea26bd876d6fc00972b3
sigurd4 <sigurd-s@online.no>
2026-09-15 03:34:55 +0200

    Remove unused import

--- src/range/range.rs
+++ src/range/range.rs
@@ -2,7 +2,7 @@ use core::{marker::Destruct, ops::Try};
 
 use array_trait::length::{self, Length, LengthValue};
 
-use crate::{Bulk, DoubleEndedBulk, IntoBulk, SplitBulk};
+use crate::{Bulk, DoubleEndedBulk, SplitBulk};
 
 #[derive(Debug)]
 #[must_use = "bulks are lazy and do nothing unless consumed"]
